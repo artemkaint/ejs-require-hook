eec42e4db866ca0b888a0f538918c088f2ee2ab8
artemkaint <artemkaint@nodasoft.com>
2015-10-16 17:31:34 +0300

    Implement options for templates

--- index.js
+++ index.js
@@ -3,10 +3,13 @@ var _ = require('lodash');
 var ext = '.ejs';
 
 module.exports = function(options) {
+  if (!options) {
+    options = {};
+  };
 
   var loadEjsFile = function(module, filename) {
     var source = fs.readFileSync(filename, 'utf8');
-    var answer = _.template(source);
+    var answer = _.template(source, options);
     module._compile(answer, filename);
   };
 
